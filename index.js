df47e841e07e0c83404cd9d756e681a28f12a931
HaileabT <Haileabtesfaye11@gmail.com>
2024-02-11 23:49:18 +0300

    fixed some styling and more js refactors

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const drums = document.querySelectorAll(".drum");
-
-drums.forEach((drum) => {
-  drum.addEventListener("click", buttonAnimation.bind(null, drum));
-});
-
 document.addEventListener("keypress", function (event) {
   makeSound(event.key);
 
@@ -12,54 +6,49 @@ document.addEventListener("keypress", function (event) {
 
 function makeSound(key) {
   switch (key) {
-    case "w":
+    case "q":
       new Audio("sounds/tom-1.mp3").play();
+      buttonAnimation(document.querySelector(".q"));
       break;
 
-    case "a":
+    case "w":
       new Audio("sounds/tom-2.mp3").play();
+      buttonAnimation(document.querySelector(".w"));
       break;
 
-    case "s":
-      new Audio("sounds/tom-3.mp3").play();
-      break;
-
-    case "d":
-      new Audio("sounds/tom-4.mp3").play();
-      break;
-
-    case "j":
+    case "e":
       new Audio("sounds/snare.mp3").play();
+      buttonAnimation(document.querySelector(".e"));
       break;
 
-    case "k":
-      new Audio("sounds/crash.mp3").play();
-      break;
-
-    case "l":
-      new Audio("sounds/kick-bass.mp3").play();
-      break;
     case "r":
+      new Audio("sounds/crash.mp3").play();
+      buttonAnimation(document.querySelector(".r"));
+      break;
+
+    case "t":
+      new Audio("sounds/kick-bass.mp3").play();
+      buttonAnimation(document.querySelector(".t"));
+      break;
+    case "y":
       new Audio("sounds/ride.mp3").play();
+      buttonAnimation(document.querySelector(".y"));
+      break;
+    case "u":
+      new Audio("sounds/hit-hat-closed.mp3").play();
+      buttonAnimation(document.querySelector(".u"));
       break;
     case "i":
-      new Audio("sounds/hit-hat-closed.mp3").play();
-      break;
-    case "f":
       new Audio("sounds/floor-tom.mp3").play();
-      break;
-    case "g":
-      new Audio("sounds/mid-tom.mp3").play();
+      buttonAnimation(document.querySelector(".i"));
       break;
   }
 }
 
-function buttonAnimation(currentKey) {
-  const activeButton = document.querySelector("." + currentKey);
-
-  activeButton.classList.add("pressed");
+function buttonAnimation(currentBtn) {
+  currentBtn.classList.add("pressed");
 
   setTimeout(function () {
-    activeButton.classList.remove("pressed");
+    currentBtn.classList.remove("pressed");
   }, 100);
 }
